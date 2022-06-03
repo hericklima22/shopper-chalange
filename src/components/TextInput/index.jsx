9b11cd97c9eba55b466648f62971175c2bb2912b
hericklima22 <herick.lima77@gmail.com>
2022-06-03 11:04:53 -0300

    home page form

--- src/components/TextInput/index.jsx
+++ src/components/TextInput/index.jsx
@@ -3,7 +3,8 @@ import './styles.css'
 export function TextInput(props) {
   return (
     <div className='container'>
-      <input type={props.type} />
+      <h3 className='label'>{props.text || ''}</h3>
+      <input type={props.type} className='input-text' />
     </div>
   )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,14 +5,17 @@ export function Home() {
   return (
     <div className="main-container">
       <div className="center-container">
+        <div className='top-text'>
+          <h1>Formulário de pedidos</h1>
+        </div>
         <div className='form'>
-          <TextInput type={'text'} />
-          <TextInput type={'text'} />
-          <TextInput type={'text'} />
+          <TextInput type={'text'} text='Nome' />
+          <TextInput type={'text'} text='Data da entrega' />
+          <TextInput type={'text'} text='Lista de compras' />
         </div>
         <div className='form-buttons'>
-          <button className='button-container'>Clique aqui</button>
-          <button className='button-container'>Clique aqui</button>
+          <input type={'button'} className='button-close' value="Cancelar" />
+          <input type={'button'} className='button-done' value="Confirmar"/>
         </div>
       </div>
     </div>
